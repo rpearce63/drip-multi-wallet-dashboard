98f69c326663867ad35ff9b68020da1213e377fc
Rick Pearce <rpearce63@mac.com>
2022-01-14 15:07:59 -0600

    update styles for header

--- src/Header.jsx
+++ src/Header.jsx
@@ -31,34 +31,25 @@ const Header = () => {
   }, []);
 
   return (
-    <nav className="navbar navbar-dark fixed-top bg-dark p-0 shadow">
-      <div className="navbar-brand">
-        <Link to={"/"}>Drip Multi-Wallet Dashboard</Link>
-      </div>
-      <div className="prices">
-        <span className="price">
-          Drip: ${formatCurrency(convertDrip(dripPrice))}
-        </span>
-        <span className="price">BNB: ${formatCurrency(bnbPrice)}</span>
-        <span className="price">Supply: {convertDrip(tokenBalance)}</span>
-        <span className="price">BR34P: {formatCurrency(br34pPrice)}</span>
-        <span className="price">REV: {formatCurrency(revPrice)}</span>
+    <nav className="navbar navbar-expand-lg nav-wrap fixed-top navbar-dark bg-dark">
+      <div className="container-fluid">
+        <div className="navbar-brand">
+          <Link to={"/"}>Drip Multi-Wallet Dashboard</Link>
+        </div>
+        <div className="prices">
+          <span className="price">
+            Drip: ${formatCurrency(convertDrip(dripPrice))}
+          </span>
+          <span className="price">BNB: ${formatCurrency(bnbPrice)}</span>
+          <span className="price">Supply: {convertDrip(tokenBalance)}</span>
+          <span className="price">BR34P: {formatCurrency(br34pPrice)}</span>
+          <span className="price">REV: {formatCurrency(revPrice)}</span>
+        </div>
 
-        {/* <div>
-        <small className="pause">Auto Refresh</small>
-        <input
-          type="checkbox"
-          checked={autoRefresh}
-          onClick={() => setAutoRefresh(!autoRefresh)}
-        />
-      </div> */}
-      </div>
-
-      <div className="card-body">
-        <h6 className="card-subtitle text-white">
+        <div className="navbar-text text-white">
           If you find this tool useful, feel free to drop me a little Drip or
           BNB: 0x645Dc8a64046FD877b82caB077BF929c299D5A7a
-        </h6>
+        </div>
       </div>
     </nav>
   );
