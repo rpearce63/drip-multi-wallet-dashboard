37743fa0c701372159d5ed352edaee56517e0bf3
Rick Pearce <rpearce63@yahoo.com>
2022-03-09 14:18:22 -0600

    add new wallet 0xe34a...

--- src/configs/dripconfig.js
+++ src/configs/dripconfig.js
@@ -1739,6 +1739,7 @@ export const adminWallets = [
   { addr: "0x9d288a8fd4c53d5a48131a3ef26e22980e7bc7dc" },
   { addr: "0xdb18efb00d9741b1ae63a6daba10ab055ecacdb4" },
   { addr: "0x9900e8113d9702c7b830859527440ca002e5f826" },
+  { addr: "0xe34a3625cab0173b63668077401dcc99375f8a8b" },
   { addr: "0x768d6f4e42e26d13daf78fd148c70367714aaf50" },
   { addr: "0x2d2929e9de3a97ac1602caf73bfe2e0f18b538bf" },
 ];
