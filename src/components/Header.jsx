89cb0d45a9421d5e2da1c33e435567b2958f3058
Rick Pearce <rpearce63@mac.com>
2022-08-01 23:18:48 -0500

    wrap price groups in header for small screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -91,95 +91,99 @@ const Header = () => {
           )}
         </div>
         <div className="prices">
-          <div className="price">
-            <a
-              href="https://bscscan.com/token/0x20f663cea80face82acdfa3aae6862d246ce0333"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Drip:
-            </a>
-            <div className="stack">
-              <div
-                className={
-                  dripPcsPrice * BUY_SPREAD >= convertDrip(dripPrice)
-                    ? "buy-dex"
-                    : ""
-                }
-              >
-                <a
-                  href="https://drip.community/fountain"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  DEX:
-                </a>
-                {formatCurrency(convertDrip(dripPrice))}
-              </div>
-              <div
-                className={
-                  dripPcsPrice * BUY_SPREAD < convertDrip(dripPrice)
-                    ? "buy-pcs"
-                    : ""
-                }
-              >
-                <a
-                  href="https://pancakeswap.finance/swap?outputCurrency=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&inputCurrency=0x20f663CEa80FaCE82ACDFA3aAE6862d246cE0333"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  PCS:
-                </a>
-                {formatCurrency(dripPcsPrice)}
-              </div>
-            </div>
-          </div>
-          <div className="price stack">
-            <label>BNB/DRIP:</label> {parseFloat(dripBnbPrice).toFixed(5)}
-          </div>
-          <div className="price stack">
-            <a
-              href="https://bscscan.com/token/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
-              target="_blank"
-              rel="noreferrer"
-            >
-              BNB:
-            </a>{" "}
-            <div className="">{formatCurrency(bnbPrice)}</div>
-          </div>
-          <div className="price stack">
-            <label>DRIP Supply:</label> {convertDrip(tokenBalance)}
-          </div>
-          <div className="price stack">
-            <a
-              href="https://v1exchange.pancakeswap.finance/#/swap?inputCurrency=0xa86d305A36cDB815af991834B46aD3d7FbB38523&outputCurrency=0xe9e7cea3dedca5984780bafc599bd69add087d56"
-              target="_blank"
-              rel="noreferrer"
-            >
-              BR34P:
-            </a>{" "}
-            {formatCurrency(br34pPrice)}
-          </div>
-          <div className="price stack">
-            <div>
+          <div className="price-group">
+            <div className="price">
               <a
-                href="https://poocoin.app/tokens/0x9a3321e1acd3b9f6debee5e042dd2411a1742002"
+                href="https://bscscan.com/token/0x20f663cea80face82acdfa3aae6862d246ce0333"
                 target="_blank"
                 rel="noreferrer"
               >
-                AFP:
-              </a>{" "}
-              {formatCurrency(pigPrice)}
+                Drip:
+              </a>
+              <div className="stack">
+                <div
+                  className={
+                    dripPcsPrice * BUY_SPREAD >= convertDrip(dripPrice)
+                      ? "buy-dex"
+                      : ""
+                  }
+                >
+                  <a
+                    href="https://drip.community/fountain"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    DEX:
+                  </a>
+                  {formatCurrency(convertDrip(dripPrice))}
+                </div>
+                <div
+                  className={
+                    dripPcsPrice * BUY_SPREAD < convertDrip(dripPrice)
+                      ? "buy-pcs"
+                      : ""
+                  }
+                >
+                  <a
+                    href="https://pancakeswap.finance/swap?outputCurrency=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&inputCurrency=0x20f663CEa80FaCE82ACDFA3aAE6862d246cE0333"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    PCS:
+                  </a>
+                  {formatCurrency(dripPcsPrice)}
+                </div>
+              </div>
             </div>
-            <div>
+            <div className="price stack">
+              <label>BNB/DRIP:</label> {parseFloat(dripBnbPrice).toFixed(5)}
+            </div>
+            <div className="price stack">
               <a
-                href="https://bscscan.com/address/0xDBdC73B95cC0D5e7E99dC95523045Fc8d075Fb9e"
+                href="https://bscscan.com/token/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
                 target="_blank"
                 rel="noreferrer"
               >
-                Dog:
+                BNB:
               </a>{" "}
-              {formatCurrency(dogPrice)}
+              <div className="">{formatCurrency(bnbPrice)}</div>
+            </div>
+          </div>
+          <div className="price-group">
+            <div className="price stack">
+              <label>DRIP Supply:</label> {convertDrip(tokenBalance)}
+            </div>
+            <div className="price stack">
+              <a
+                href="https://v1exchange.pancakeswap.finance/#/swap?inputCurrency=0xa86d305A36cDB815af991834B46aD3d7FbB38523&outputCurrency=0xe9e7cea3dedca5984780bafc599bd69add087d56"
+                target="_blank"
+                rel="noreferrer"
+              >
+                BR34P:
+              </a>{" "}
+              {formatCurrency(br34pPrice)}
+            </div>
+            <div className="price stack">
+              <div>
+                <a
+                  href="https://poocoin.app/tokens/0x9a3321e1acd3b9f6debee5e042dd2411a1742002"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  AFP:
+                </a>{" "}
+                {formatCurrency(pigPrice)}
+              </div>
+              <div>
+                <a
+                  href="https://bscscan.com/address/0xDBdC73B95cC0D5e7E99dC95523045Fc8d075Fb9e"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Dog:
+                </a>{" "}
+                {formatCurrency(dogPrice)}
+              </div>
             </div>
           </div>
         </div>
