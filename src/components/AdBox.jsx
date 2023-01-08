33666ecea5e7cfc52389e68c60e6ba365414bab6
Rick Pearce <rpearce63@yahoo.com>
2023-01-08 11:18:21 -0600

    edit image url

--- src/components/AdBox.jsx
+++ src/components/AdBox.jsx
@@ -4,7 +4,7 @@ const AdBox = () => {
       <div className="card-body">
         <a href="https://t.me/dripreservoir" target="_blank" rel="noreferrer">
           <img
-            src="https://cdn.glitch.global/24255909-08cf-404f-ad48-b6fb2de88697/dashboard-ad.jpg?v=1673196971727"
+            src="https://cdn.glitch.global/24255909-08cf-404f-ad48-b6fb2de88697/dashboard-ad.jpg"
             alt="StuffTheRez"
             width="500px"
           />
