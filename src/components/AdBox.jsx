08e3d7fe528cfca56516cf1a12b93b6c248ede5e
Rick Pearce <rpearce63@yahoo.com>
2023-01-17 08:10:23 -0600

    add total minted to header

--- src/components/AdBox.jsx
+++ src/components/AdBox.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 const AdBox = () => {
+  const [imageLoaded, setImageLoaded] = useState(true);
+
   return (
-    <div className="drip-ads card">
+    <div className={`drip-ads card ${imageLoaded || "hide"}`}>
       <div className="card-body">
         <a href="https://t.me/dripreservoir" target="_blank" rel="noreferrer">
           <img
             src="https://cdn.glitch.global/24255909-08cf-404f-ad48-b6fb2de88697/dashboard-ad.jpg"
+            onError={() => setImageLoaded(false)}
             alt="StuffTheRez"
             width="500px"
           />
