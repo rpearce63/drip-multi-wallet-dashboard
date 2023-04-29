7917264bc3bd0ea9317fef750200ba62b667ff3a
Rick Pearce <rpearce63@yahoo.com>
2023-04-29 10:08:22 -0500

    update ad image url to s3 bucket

--- src/components/AdBox.jsx
+++ src/components/AdBox.jsx
@@ -7,7 +7,7 @@ const AdBox = () => {
       <div className="card-body">
         <a href="https://t.me/dripreservoir" target="_blank" rel="noreferrer">
           <img
-            src="https://cdn.glitch.global/24255909-08cf-404f-ad48-b6fb2de88697/dashboard-ad.jpg"
+            src="https://drip-mw-dashboard.s3.amazonaws.com/ads/dashboard-ad.jpg"
             onError={() => setImageLoaded(false)}
             alt="StuffTheRez"
             width="500px"
